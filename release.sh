bf0f8f65faca3af768a82adb20cb44618454d3ac
Baptiste Jonglez <git@bitsofnetworks.org>
2021-01-08 18:42:33 +0100

    Update release script to account for message build changes

--- release.sh
+++ release.sh
@@ -2,18 +2,15 @@
 
 set -e
 
-MESSAGES="s-messages src/messages.h src/messages.cc"
 RELEASE="$1"
 
 [ -z "$RELEASE" ] && { echo "usage: $0 X.Y.Z"; exit 1; }
 
-make clean
-make src/messages.h src/messages.cc
+# Check repo is clean
+git update-index -q --refresh
+git diff-files --quiet || { echo "error: ensure git repo has no changes"; exit 1; }
 
-git add -f $MESSAGES
-git commit -m "Release $RELEASE"
+# TODO: regenerate message files to make sure they are up-to-date
+
 git tag v"$RELEASE"
 git archive --prefix=kea-hook-runscript-"$RELEASE"/ -o kea-hook-runscript-"$RELEASE".tar.gz v"$RELEASE"
-git rm $MESSAGES
-git commit -m "Go back to development after $RELEASE release"
-
